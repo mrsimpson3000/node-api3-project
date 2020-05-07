9f4d084914fbb9a3c5169e4a544ada8bbb3ba34a
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-07 09:11:50 -0400

    Second custom middleware written.

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,50 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const Users = require("./userDb");
+const Posts = require("../posts/postDb");
+
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
 //custom middleware
-
+// validate user id / if valid store as req.user / else res.status(400).json({ message: "invalid user id" })
 function validateUserId(req, res, next) {
-  // do your magic!
+  Users.getById(req.params.id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({ message: "invalid user id" });
+    });
 }
 
 function validateUser(req, res, next) {
