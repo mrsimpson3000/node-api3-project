87328dc2abd874d8c69820c11bd445bcc2b444d2
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-06 22:06:59 -0400

    Logger middleware is done. Routing to posts and users is set up. If I have time I'll write the endpoints for posts.

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 
+const postRouter = require("./posts/postRouter");
+
+const userRouter = require("./users/userRouter");
+
 const server = express();
 
-server.get("/", (req, res) => {
+server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use("/api/users", logger, userRouter);
+
+server.use("/api/posts", logger, postRouter);
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${Date()} ${req.method} to ${req.url}`);
+}
 
 module.exports = server;
